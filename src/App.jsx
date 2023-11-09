e97f120f44bcf46ef712c5f8db9c4d116c564fe3
Weiye Wang <146437348+LucasWWY@users.noreply.github.com>
2023-11-09 11:51:07 +0000

    Commit made from VS Code Azure Static Web Apps

--- src/App.jsx
+++ src/App.jsx
@@ -24,14 +24,14 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
+  name: "Lucas (weiye) Wang",
+  title: "Back end Developer & Data Analyst",
+  email: "lucas@email.com",
+  gitHub: "xxx",
+  instagram: "xxx",
   linkedIn: "satyanadella",
   medium: "",
-  twitter: "microsoft",
+  twitter: "xxx",
   youTube: "Code",
 };
 
